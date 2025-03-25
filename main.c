b92e3787b49c620f3748dd38cec38eed74eeaa4e
Dax0s <dev@dklimavicius.com>
2025-03-25 18:40:24 +0200

    Fix clients disconnecting and make chat server work with email server

--- main.c
+++ main.c
@@ -21,9 +21,18 @@ void error(const char *msg)
     exit(1);
 }
 
+int starts_with(const char* str, const char* prefix)
+{
+    return strncmp(str, prefix, strlen(prefix));
+}
+
 void signal_handler(int);
 
 volatile int keep_running = 1;
+
+void parse_server(const char* str, char* server_name);
+void parse_user(const char* str, char* user_name);
+void parse_msg(const char* str, char* msg);
 
 int main(const int argc, const char **argv)
 {
@@ -41,6 +50,28 @@ int main(const int argc, const char **argv)
         free(tmp);
     }
 
+    struct sockaddr_in email_server_addr;
+    const int email_server_port = atoi(argv[3]);
+
+    const int email_server_socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (email_server_socket_fd < 0)
+        error("ERROR opening socket");
+
+    struct hostent *server = gethostbyname("localhost");
+    if (server == NULL) {
+        fprintf(stderr, "ERROR, no such host\n");
+        exit(0);
+    }
+
+    email_server_addr.sin_family = AF_INET;
+    bcopy(server->h_addr, (char *)&email_server_addr.sin_addr.s_addr, server->h_length);
+    email_server_addr.sin_port = htons(email_server_port);
+
+    if (connect(email_server_socket_fd, (struct sockaddr *) &email_server_addr, sizeof(email_server_addr)) < 0)
+        error("ERROR connecting to email server");
+
+    write(email_server_socket_fd, argv[1], strlen(argv[1]));
+
     char buffer[1024];
 
     struct addrinfo hints, *server_addr, *p;
@@ -55,7 +86,7 @@ int main(const int argc, const char **argv)
         error("getaddrinfo error");
     }
 
-    int sock;
+    int sock = -1;
     for (p = server_addr; p != NULL; p = p->ai_next)
     {
         if ((sock = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1)
@@ -70,7 +101,7 @@ int main(const int argc, const char **argv)
         break;
     }
 
-    if (p == NULL)
+    if (p == NULL || sock == -1)
         error("failed to bind");
 
     listen(sock, 5);
@@ -130,27 +161,26 @@ int main(const int argc, const char **argv)
                     printf("Client with socket fd %d and username %s disconnected\n", clients[i].sock, clients[i].username);
 
                     close(client_pollfd[i].fd);
-                    for (int j = i; j < connected_clients - 1; j++)
+                    connected_clients--;
+                    for (int j = i; j < connected_clients; j++)
                     {
                         client_pollfd[j] = client_pollfd[j + 1];
                         clients[j] = clients[j + 1];
-
-                        connected_clients--;
-
-                        struct pollfd* tmp = realloc(client_pollfd, sizeof(struct pollfd) * connected_clients);
-                        if (tmp == NULL)
-                        {
-                            error("failed to allocate memory");
-                        }
-                        client_pollfd = tmp;
-
-                        client* tmp1 = realloc(clients, sizeof(client) * connected_clients);
-                        if (tmp1 == NULL)
-                        {
-                            error("failed to allocate memory");
-                        }
-                        clients = tmp1;
                     }
+
+                    struct pollfd* tmp = realloc(client_pollfd, sizeof(struct pollfd) * connected_clients);
+                    if (tmp == NULL)
+                    {
+                        error("failed to allocate memory");
+                    }
+                    client_pollfd = tmp;
+
+                    client* tmp1 = realloc(clients, sizeof(client) * connected_clients);
+                    if (tmp1 == NULL)
+                    {
+                        error("failed to allocate memory");
+                    }
+                    clients = tmp1;
                 }
                 else if (client_pollfd[i].revents & POLLIN)
                 {
@@ -183,6 +213,51 @@ int main(const int argc, const char **argv)
 
                         continue;
                     }
+                    if (starts_with(buffer, "@get") == 0)
+                    {
+                        char get_buffer[1024] = "";
+                        sprintf(get_buffer, "@get <%s>", clients[i].username);
+                        write(email_server_socket_fd, get_buffer, strlen(get_buffer));
+
+                        char email_buffer[1024] = "";
+                        while (1)
+                        {
+                            read(email_server_socket_fd, email_buffer, 1023);
+                            if (starts_with(email_buffer, "@end") == 0)
+                            {
+                                break;
+                            }
+
+                            char tmp_buffer[1024] = "";
+                            sprintf(tmp_buffer, "PRANESIMAS %s", email_buffer);
+                            write(clients[i].sock, tmp_buffer, strlen(tmp_buffer));
+                            bzero(tmp_buffer, sizeof(tmp_buffer));
+                            bzero(email_buffer, sizeof(email_buffer));
+                            write(email_server_socket_fd, "@received\n", 10);
+                        }
+
+                        continue;
+                    }
+
+                    if (starts_with(buffer, "@send") == 0)
+                    {
+                        char server_name[1024] = "";
+                        char user_name[1024] = "";
+                        char msg[1024] = "";
+
+                        parse_user(buffer, user_name);
+                        parse_server(buffer, server_name);
+                        parse_msg(buffer, msg);
+
+                        char send_buffer[1024] = "";
+
+                        sprintf(send_buffer, "@send <%s> <%s> <%s>: %s", clients[i].username, server_name, user_name, msg);
+
+                        write(email_server_socket_fd, send_buffer, strlen(send_buffer));
+
+                        continue;
+                    }
+
                     for (int j = 0; j < connected_clients; j++)
                     {
                         if (i == j)
@@ -219,6 +294,76 @@ int main(const int argc, const char **argv)
     free(client_pollfd);
 
     return 0;
+}
+
+void parse_server(const char* str, char* server_name)
+{
+    bzero(server_name, sizeof(server_name));
+
+    int write = 0;
+    int len = 0;
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] == '<')
+        {
+            write++;
+            continue;
+        }
+        if (str[i] == '>')
+        {
+            break;
+        }
+        if (write == 1)
+        {
+            server_name[len++] = str[i];
+        }
+    }
+}
+
+void parse_user(const char* str, char* user_name)
+{
+    bzero(user_name, sizeof(user_name));
+
+    int write = 0;
+    int len = 0;
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] == '<')
+        {
+            write++;
+            continue;
+        }
+        if (str[i] == '>' && write == 2)
+        {
+            break;
+        }
+        if (write == 2)
+        {
+            user_name[len++] = str[i];
+        }
+    }
+}
+
+void parse_msg(const char* str, char* msg)
+{
+    bzero(msg, sizeof(msg));
+
+    int write = 0;
+    int len = 0;
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] == '>')
+        {
+            write++;
+            continue;
+        }
+        if (write == 3)
+        {
+            msg[len++] = str[i];
+        }
+        if (write == 2)
+            write++;
+    }
 }
 
 void signal_handler(int)
